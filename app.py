008b2e99d3b393b4b9b13d7712b10975547d3685
yongqiang.ren <yongqiang.ren@ele.me>
2016-07-26 20:50:37 +0800

    add hello handler

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def index():
     print("hello")
     return '<h1>hello world</h1>'
 
+
+@app.route('/hello/<name>')
+def hello(name):
+    return '<h1>hello {}</h1>'.format(name)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
